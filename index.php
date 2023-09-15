d8a6007b5ce16e3d3c75e39549e06f7199130ac9
MinhazUddin25 <hafijminhaz@gmail.com>
2023-09-16 01:14:43 +0600

    PHP Prectice

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+// $name = "HEllo";
+// $age = 12;
+// var_dump($age);
+
+// var_dump($name);
+
+// $name1= "Minhaz";
+// $name2= "Uddin";
+// var_dump($name1+$name2);
+
+
+$result = true;
+var_dump($result);
+$name = "Earth";
+echo "We\'re living on {$name}";
